4266de8dd5785ce11550b1ffe69525f6c297a624
georgepa <george.prado.a@uni.pe>
2020-06-30 00:52:11 -0500

    section3-UsingNodeModulesSystem

--- app.js
+++ app.js
@@ -1,53 +1,10 @@
 const http = require('http');
-const fs = require('fs');
 // node application usa un enfoque impulsado por eventos para todo tipo de cosas
 //se utiliza el concepto de bucle de eventos o event loop, en multiples request es muy rapido en el manejo
 // de solicitudes y en realidad de manera background realiza algunos subprocesos multiples al aprovechar el sistema operativo
-const server = http.createServer((req, res) => {
-    //console.log(req);
-    //console.log('se hizo un request.. se abrio un hilo para la ejecución');
-    //console.warn(req.method, req.headers);
-    //process.exit(); //sentencia que mata el hilo principal del server node.. solo se ejecuta una vez cuando se hace un request
-    const url = req.url;
-    const method = req.method; //obtiene el metodo por el cual fue llamado el servidor
-    if(url === '/'){
-        res.write('<html>');
-        res.write('<head><title>Enter Message</title></head>');
-        res.write('<body><form action="/message" method="POST"><input type="text" name="msg"><button type="submit">Enviar</button></form></body>');
-        res.write('</html>');
-        return res.end();
-    }
+const routes = require('./routes');
 
-    if(url ==='/message' && method === 'POST'){
-        const body = [];
-        //va leyendo los eventos del flujo de datos del request
-        req.on('data', (chunk) => {
-            console.log(chunk);
-            body.push(chunk);
-        });
-        return req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString();
-            const message = parsedBody.split('=')[1];
-            //esta manera es incorrecta, ya que para archivos pesados rompe el loop de eventos de node
-            //fs.writeFileSync('message1.txt',message);
-            //esta manera es manejable por callback ya que no bloquea el loop de eventos de node
-            fs.writeFile('message1.txt',message, err => {
-                res.statusCode = 302; //esto redirecciona de acuerdo a la cabecera
-                res.setHeader('Location','/');
-                return res.end();
-            });
-        });
-        //fs.writeFileSync('message.txt','DUMMY');
-        /*res.statusCode = 302; //esto redirecciona de acuerdo a la cabecera
-        res.setHeader('Location','/algo');
-        return res.end(); */
-    }
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>My First Page</title></head>');
-    res.write('<body><h1>Hello from Node.js Server!</h1></body>');
-    res.write('</html>');
-    res.end();
-});
+console.log(routes.someText);
+const server = http.createServer(routes.handler);
 
 server.listen(3000);
