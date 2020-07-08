7fbf2669b5d2289cff3c13403a3554bd2826fa54
georgepa <george.prado.a@uni.pe>
2020-07-07 23:55:56 -0500

    section5-handling different routes

--- app.js
+++ app.js
@@ -4,16 +4,24 @@
 const express = require('express');
 const app = express();
 
-app.use((req ,res , next) => {
-    console.log('In the middleware!!');
-    next(); //Allows the request to coninue to the next middleware in line
+app.use('/',(req ,res , next) => {
+    console.log('This always runs!');
+    next();
+    //next(); //Allows the request to coninue to the next middleware in line
     //app.use abre un canal o middleware para solicitar o escuchar algun recurso
 });
 
-app.use((req ,res , next) => {
+app.use('/add-product',(req ,res , next) => {
     console.log('In another middleware!!');
     // ... doesn't neccesary ste header for html
+    res.send('<h1>The product page</h1>')
+});
+
+app.use('/',(req ,res , next) => {
+    console.log('In the middleware to root!!');
     res.send('<h1>Hello from Express!!</h1>')
+    //next(); //Allows the request to coninue to the next middleware in line
+    //app.use abre un canal o middleware para solicitar o escuchar algun recurso
 });
 
 // express have a embebed http to listen port
