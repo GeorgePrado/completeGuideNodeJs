0877bc47f82fa410ae7695ac290adf56c46140d3
georgepa <george.prado.a@uni.pe>
2020-07-08 02:20:33 -0500

    section5-using helper function for navigation

--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const path = require('path');
+const rootDir = require('../util/path');
 const router = express.Router();
 
 // /admin/add-product => GET
 router.get('/add-product',(req ,res , next) => {
     console.log('This register a product!!');
     //res.send('<form action="/admin/product" method="POST"><input type="text" name="product"><button type="submit">Add product</button></form>');
-    res.sendFile(path.join(__dirname, '../', 'views','add-product.html'));
+    //res.sendFile(path.join(__dirname, '../', 'views','add-product.html'));
+    res.sendFile(path.join(rootDir, 'views','add-product.html'));
 });
 
 // /admin/product => POST
--- routes/shop.js
+++ routes/shop.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const path = require('path');
+const rootDir = require('../util/path');
 const router = express.Router();
 
 router.get('/',(req ,res , next) => {
     console.log('In the middleware to root!!');
     //res.send('<h1>Hello from Express!!</h1>')
-    res.sendFile(path.join(__dirname, '../', 'views','shop.html'));
+    //res.sendFile(path.join(__dirname, '../', 'views','shop.html'));
+    res.sendFile(path.join(rootDir, 'views','shop.html'));
     //next(); //Allows the request to coninue to the next middleware in line
     //app.use abre un canal o middleware para solicitar o escuchar algun recurso
 });
