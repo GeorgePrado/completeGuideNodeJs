b9ab2ead99c50b38009b85c7c6f7a1d84486abb4
georgepa <george.prado.a@uni.pe>
2020-07-07 23:35:32 -0500

    section5-middleware work

--- app.js
+++ app.js
@@ -2,13 +2,25 @@ const http = require('http');
 // node application usa un enfoque impulsado por eventos para todo tipo de cosas
 //se utiliza el concepto de bucle de eventos o event loop, en multiples request es muy rapido en el manejo
 // de solicitudes y en realidad de manera background realiza algunos subprocesos multiples al aprovechar el sistema operativo
-const routes = require('./routes');
+const express = require('express');
+const app = express();
 
-console.log(routes.someText);
-const server = http.createServer(routes.handler);
+app.use((req ,res , next) => {
+    console.log('In the middleware!!');
+    next(); //Allows the request to coninue to the next middleware in line
+    //app.use abre un canal o middleware para solicitar o escuchar algun recurso
+});
+
+app.use((req ,res , next) => {
+    console.log('In another middleware!!');
+    // ... doesn't neccesary ste header for html
+    res.send('<h1>Hello from Express!!</h1>')
+});
+
+const server = http.createServer(app);
 
 server.listen(3000);
 //to install new package you should use 
-// npm install <namePackage> --save      ... in production
+// npm install <namePackage> --save      ... for production and develop
 // npm install <namePackage> --save--dev ... only development environment
 // npm install <namePackage> -g          ... global enviroment
