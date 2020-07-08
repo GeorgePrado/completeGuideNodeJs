3c12bcd3306cdaf484aea495af552b95b1ac21a5
georgepa <george.prado.a@uni.pe>
2020-07-08 01:19:19 -0500

    section5-filtering paths

--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
+// /admin/add-product => GET
 router.get('/add-product',(req ,res , next) => {
     console.log('This register a product!!');
-    res.send('<form action="/product" method="POST"><input type="text" name="product"><button type="submit">Add product</button></form>');
+    res.send('<form action="/admin/product" method="POST"><input type="text" name="product"><button type="submit">Add product</button></form>');
 });
 
+// /admin/product => POST
 //limitamos el middleware para product en solo dar la opcion a request del tipo POST
 //app.use('/product', (req ,res , next) => {
 router.post('/product', (req ,res , next) => {
